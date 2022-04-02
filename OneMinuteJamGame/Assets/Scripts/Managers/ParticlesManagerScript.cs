8dce51dca94a438cb5291aa6f056e92fb22a911c
Taiyo <taiyoperson@gmail.com>
2022-04-02 13:18:49 +1300

    Added damaged particles

---
+++ OneMinuteJamGame/Assets/Scripts/Managers/ParticlesManagerScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesManagerScript : MonoBehaviour
+{
+    [SerializeField] GameObject damagedParticles;
+    [SerializeField] Transform playerTransform;
+
+    void SpawnDamagedParticles()
+    {
+        GameObject damagedParticlesInstance = Instantiate(damagedParticles, playerTransform);
+        GameObject.Destroy(damagedParticlesInstance, 0.4f);
+    }
+
+    private void Start()
+    {
+        healthManagerScript.OnDamaged += SpawnDamagedParticles;
+    }
+}
