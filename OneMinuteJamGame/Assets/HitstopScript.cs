15a1d93a317171f6dbe4634ba465e444df6cdefb
Taiyo <taiyoperson@gmail.com>
2022-04-06 12:38:24 +1200

    did thing

---
+++ OneMinuteJamGame/Assets/HitstopScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitstopScript : MonoBehaviour
+{
+    [SerializeField] public float pauseTime;
+
+    private void Awake()
+    {
+        healthManagerScript.OnDamaged += CallHitStop;
+    }
+
+    private void OnDestroy()
+    {
+        healthManagerScript.OnDamaged -= CallHitStop;
+    }
+
+    public void CallHitStop()
+    {
+        StartCoroutine(HitStop());
+    }
+
+    IEnumerator HitStop()
+    {
+        Time.timeScale = 0f;
+
+        yield return new WaitForSecondsRealtime(pauseTime);
+
+        Time.timeScale = 1f;
+    }
+}
